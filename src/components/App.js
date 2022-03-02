8c7bd73a093c88833486b5308bc40cd7f1b1a171
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-03-02 13:02:47 +0100

    init react

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+import { useEffect, useState } from 'react';
+// Fichero src/components/App.js
+
+import { Link, Route } from 'react-router-dom';
+
+const App = () => {
+  return <div>Hola mundo</div>;
+};
+
+export default App;
