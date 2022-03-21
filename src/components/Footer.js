1fec449507d4b1fde4ff814275228b8cfa69563c
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-03-21 10:42:19 +0100

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,14 @@
 const Footer = (props) => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__container--text">Made by Alicia G</p>
+        <p className="footer__container--text">
+          Illustration made by @laurited
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
