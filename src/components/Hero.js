9eaed4ac08fa0aec566503b5784269f11f4aa99b
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2022-03-22 11:49:04 +0100

    container

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,37 +6,42 @@ const Hero = (props) => {
     <section className="section__intro">
       <div className="section__intro--div">
         <img className="section__intro--img" src={photome} alt="" />
-        <h1 className="section__intro--title">Hola, me llamo Alicia Gallego</h1>
-        <p className="section__intro--text">y soy Front-End Developer</p>
-        <ul className="section__intro--list">
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/aliciagallegogonzalez/"
-            >
-              <FaIcons.FaLinkedin className="section__intro--icons" />
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/aliciaggz"
-            >
-              <FaIcons.FaGithubSquare className="section__intro--icons" />
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/aliciaggz"
-            >
-              <IoIcons.IoMdMail className="section__intro--icons" />
-            </a>
-          </li>
-        </ul>
+        <div className="section__intro--div1">
+          <h1 className="section__intro--title">
+            Hola, me llamo Alicia Gallego
+          </h1>
+
+          <p className="section__intro--text">y soy Front-End Developer</p>
+          <ul className="section__intro--list">
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/aliciagallegogonzalez/"
+              >
+                <FaIcons.FaLinkedin className="section__intro--icons" />
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/aliciaggz"
+              >
+                <FaIcons.FaGithubSquare className="section__intro--icons" />
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/aliciaggz"
+              >
+                <IoIcons.IoMdMail className="section__intro--icons" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="section__intro--div">
         <img src="" alt="" />
